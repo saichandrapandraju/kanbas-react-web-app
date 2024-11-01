dd94c577bcab0555494a9aa1f50173157ac63263
saichandrapandraju <saichandrapandraju@gmail.com>
2024-10-31 21:57:05 -0400

    added bootstrap for labs

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,16 +1,49 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function TOC() {
-    return (
-      <ul>
-        <li><Link to="/Labs">Labs</Link></li>
-        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        <li><Link to="/Labs/Lab4">Lab 4</Link></li>
-        <li><Link to="/Kanbas">Kanbas</Link></li>
+const TOC = () => {
+  return (
+    <div className="container mt-4">
+      <nav className="card shadow-sm">
+        <div className="card-header bg-danger text-white">
+          <h5 className="mb-0">Navigation Menu</h5>
+        </div>
+        <div className="card-body">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <Link 
+                to="/Labs" 
+                className="nav-link text-danger hover:bg-danger-100 rounded"
+              >
+                <i className="fas fa-flask me-2"></i>
+                Labs
+              </Link>
+            </li>
+            {[1, 2, 3, 4].map((labNumber) => (
+              <li className="nav-item ms-3" key={labNumber}>
+                <Link 
+                  to={`/Labs/Lab${labNumber}`}
+                  className="nav-link text-secondary hover:bg-gray-100 rounded"
+                >
+                  <i className="fas fa-angle-right me-2"></i>
+                  Lab {labNumber}
+                </Link>
+              </li>   
+            ))}
+            <li className="nav-item mt-2">
+              <Link 
+                to="/Kanbas" 
+                className="nav-link text-danger hover:bg-gray-100 rounded"
+              >
+                <i className="fas fa-tasks me-2"></i>
+                Kanbas
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-      </ul>
-    );
-  }
-  
+export default TOC;
